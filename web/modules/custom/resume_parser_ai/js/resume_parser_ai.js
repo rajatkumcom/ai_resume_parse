c68e382790b4b104fb8aeb60a26d1fa1026f44f5
Rajat Kumar Verma <rajat.verma@axelerant.com>
2024-02-08 08:00:09 +0530

    Resume parser module chnages

--- web/modules/custom/resume_parser_ai/js/resume_parser_ai.js
+++ web/modules/custom/resume_parser_ai/js/resume_parser_ai.js
@@ -1,16 +1,35 @@
 (function ($, Drupal) {
   Drupal.behaviors.resumeParserAI = {
     attach: function (context, settings) {
-      // _.once(function () {
       $('input[type="file"]').change(function () {
         $("#edit-ajax-submit").trigger("click");
       });
-      // });
+      $("#edit-field-job-role").change(function () {
+        if ($("#edit-field-job-role").val() == "_none") {
+          $("#resume-hide-job-role").hide();
+          $(".divider").hide();
+          $("#edit-footer").hide();
+          $("#edit-actions").hide();
+        } else {
+          $("#resume-hide-job-role").show();
+          $(".divider").show();
+          $("#edit-footer").show();
+          $("#edit-actions").show();
+        }
+      });
     },
   };
-  // $(document).ready(function () {
-  //   $('input[type="file"]').change(function () {
-  //     $("#edit-ajax-submit").trigger("click");
-  //   });
-  // });
+  $(document).ready(function () {
+    if ($("#edit-field-job-role").val() == "_none") {
+      $("#resume-hide-job-role").hide();
+      $(".divider").hide();
+      $("#edit-footer").hide();
+      $("#edit-actions").hide();
+    } else {
+      $("#resume-hide-job-role").show();
+      $(".divider").show();
+      $("#edit-footer").show();
+      $("#edit-actions").show();
+    }
+  });
 })(jQuery, Drupal);
